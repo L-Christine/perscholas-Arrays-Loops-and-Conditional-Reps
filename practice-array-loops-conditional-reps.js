1884131d984ca926f1e284f2e405c41fd15c5e2e
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-09-12 20:40:51 -0400

    Get Even answered

--- practice-array-loops-conditional-reps.js
+++ practice-array-loops-conditional-reps.js
@@ -10,6 +10,12 @@ for (i = 1; i <= 20; i++){
 }
 
 //Get Even
+//Write a for loop that will log only the even numbers in 0 through 200.
+for (i = 0; i <= 200; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
 
 //Fizz Buzz
 
