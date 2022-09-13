68887d5d302729e7d83397e7ab380d30c1441e68
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-09-12 20:06:53 -0400

    Easy Going answered

---
+++ practice-array-loops-conditional-reps.js
@@ -0,0 +1,28 @@
+//====Part I: HTML & CSS
+//Rate 4.5
+
+//====Part II: JavaScript Reps
+//Easy Going
+//Write a for loop that will log the numbers 1 through 20.
+
+for (i = 1; i <= 20; i++){
+    console.log(i);
+}
+
+//Get Even
+
+//Fizz Buzz
+
+//Wild Wild Life
+
+//Yell at the Ninja Turtles
+
+//Methods, Revisited
+
+//Where is Waldo
+
+//Excited Kitten
+
+//Find the Median
+
+//
