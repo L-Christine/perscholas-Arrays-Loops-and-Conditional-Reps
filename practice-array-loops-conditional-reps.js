d4412f027fd9aacd6a0169a5ab1a77ef29b96995
Christine Lee <59518411+christineslee@users.noreply.github.com>
2022-09-12 20:55:30 -0400

    Wild Wild Life answered

--- practice-array-loops-conditional-reps.js
+++ practice-array-loops-conditional-reps.js
@@ -2,14 +2,14 @@
 //Rate 4.5
 
 //====Part II: JavaScript Reps
-//Easy Going
+//====Easy Going
 //Write a for loop that will log the numbers 1 through 20.
 
 for (i = 1; i <= 20; i++){
     console.log(i);
 }
 
-//Get Even
+//====Get Even
 //Write a for loop that will log only the even numbers in 0 through 200.
 for (i = 0; i <= 200; i++){
     if (i % 2 == 0){
@@ -17,7 +17,7 @@ for (i = 0; i <= 200; i++){
     }
 }
 
-//Fizz Buzz
+//====Fizz Buzz
 for (let i = 1; i <= 100; i++){
     if (i%3 === 0 && i%5 === 0){
         console.log("FizzBuzz")
@@ -31,14 +31,34 @@ for (let i = 1; i <= 100; i++){
 }
 
 
-//Wild Wild Life
+//====Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
 
-//Yell at the Ninja Turtles
+plantee[2] = 5001
 
-//Methods, Revisited
+// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
 
-//Where is Waldo
+wolfy[3] = "Gotham City"
+
+// Give D'Art a second hometown by adding "Hawkins"
+
+dart.push("Hawkins")
+
+// Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
+
+wolfy.splice(0, 1, "Gameboy")
+
+//====Yell at the Ninja Turtles
+
+//====Methods, Revisited
+
+//====Where is Waldo
 
 //Excited Kitten
 
